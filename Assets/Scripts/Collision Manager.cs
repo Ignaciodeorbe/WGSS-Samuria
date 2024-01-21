33958d7317eabebaafbab2829e689ce0d3cb193a
jlw6563 <jlw6563@MSS2175-10>
2024-01-20 19:55:32 -0500

    Bugs messy needs fix

--- Assets/Scripts/Collision Manager.cs
+++ Assets/Scripts/Collision Manager.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class CollisionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private SpriteRenderer srender_hands;
 
+    [SerializeField]
+    private SpriteRenderer srender_sword;
+
+
+    [SerializeField]
+    private SpriteRenderer srender_yinyang;
+
+    [SerializeField]
+    SwordMovement movement;
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        srender_sword.color = Color.red;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            movement.TranslationSpeed = 0;
+            movement.RotationSpeed = 0;
+            srender_hands.enabled = true;
+            srender_yinyang.enabled = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        srender_sword.color = Color.white;
     }
 }
+
+    
--- Assets/Scripts/SwordMovement.cs
+++ Assets/Scripts/SwordMovement.cs
@@ -10,9 +10,14 @@ public class SwordMovement : MonoBehaviour
     float rotationSpeed = 0.25f;
 
 
-    Vector3 translationVal = new Vector3(2,-1,0);
+    Vector3 translationVal = new Vector3(1.5f,-1,0);
 
     Vector3 spin = new Vector3(0,0,-.05f);
+
+    public int TranslationSpeed { set { translationSpeed = value; } }
+    
+    public float RotationSpeed { set { rotationSpeed = value; } }
+
 
     // Update is called once per frame
     void Update()
@@ -31,16 +36,18 @@ public class SwordMovement : MonoBehaviour
         //Increases position by translation val which is going to the right and the down times the speed
         //Rotates the the z by spin and the speed.
         transform.position += translationVal * translationSpeed * Time.deltaTime;
-        transform.Rotate(spin * rotationSpeed);
+        //transform.Rotate(spin * rotationSpeed * Time.deltaTime);
     }
 
 
     /// <summary>
     /// Resets the position of the sword
     /// </summary>
-    public void ResetPos()
+    public void ResetInfo()
     {
         transform.position = new Vector3(-13,2,0);
         transform.rotation = Quaternion.Euler(0,0,-33);
-    }
+        rotationSpeed = 0.25f;
+        translationSpeed = 1;
+    } 
 }
